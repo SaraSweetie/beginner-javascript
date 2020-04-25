eb8957f0a4b60fcb0a048b5d8c6cef6ee18c5270
Sara Krum <sarasmith887@gmail.com>
2020-04-25 13:52:19 -0400

    function setup, argument, parapemter, default value

--- playground/custom-functions/cf-sarasweetie.js
+++ playground/custom-functions/cf-sarasweetie.js
@@ -1,1 +1,8 @@
-console.log('it works!');
+function calculateBill(billAmount, taxRate, tipRate = 0.15) {
+  const total = billAmount + (billAmount * taxRate) + (billAmount * tipRate);
+
+  return total;
+}
+
+const billTotal = `The total bill is $${calculateBill(100, 0.13, undefined)}.`;
+console.log(billTotal);
