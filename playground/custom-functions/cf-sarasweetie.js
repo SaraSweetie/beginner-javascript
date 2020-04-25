42ccb6856bc8527d48feb3552b29126bd79c905a
Sara Krum <sarasmith887@gmail.com>
2020-04-25 13:54:56 -0400

    undo

--- playground/custom-functions/cf-sarasweetie.js
+++
@@ -1,8 +0,0 @@
-function calculateBill(billAmount, taxRate, tipRate = 0.15) {
-  const total = billAmount + (billAmount * taxRate) + (billAmount * tipRate);
-
-  return total;
-}
-
-const billTotal = `The total bill is $${calculateBill(100, 0.13, undefined)}.`;
-console.log(billTotal);
